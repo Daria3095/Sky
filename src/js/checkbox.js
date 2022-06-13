3f0f7cc2d718721e8b98324146dde65d4be54fab
Касенюк Дарья Александровна <d.kaseniuk@cons.local>
2022-06-13 20:05:43 +0300

    first commit

---
+++ src/js/checkbox.js
@@ -0,0 +1,95 @@
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes0() {
+  var checkboxes = document.getElementById("checkboxes0");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
+
+function showCheckboxes1() {
+  var checkboxes = document.getElementById("checkboxes1");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
+
+function showCheckboxes2() {
+  var checkboxes = document.getElementById("checkboxes2");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
+
+function showCheckboxes3() {
+  var checkboxes = document.getElementById("checkboxes3");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
+
+function showCheckboxes4() {
+  var checkboxes = document.getElementById("checkboxes4");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
+
+function showCheckboxes5() {
+  var checkboxes = document.getElementById("checkboxes5");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
+
+function showCheckboxes6() {
+  var checkboxes = document.getElementById("checkboxes6");
+
+  if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+  } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+  }
+}
